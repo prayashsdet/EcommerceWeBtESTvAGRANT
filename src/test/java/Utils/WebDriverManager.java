f945e96050663c76c29ac30555af2d855ec0e739
prayashsdet <prayashqa@gmail.com>
2024-03-12 20:02:20 +0530

    c

--- src/test/java/Utils/WebDriverManager.java
+++ src/test/java/Utils/WebDriverManager.java
@@ -4,6 +4,13 @@ import org.openqa.selenium.WebDriver;
 
 /**
  * Factory class for creating WebDriver instances based on the specified browser name.
+ * This class demonstrates both the Factory Method and Singleton patterns.
+ * The Factory Method pattern is used to provide an interface for creating objects, allowing subclasses
+ * to alter the type of objects that will be created. In this case, concrete subclasses (ChromeDriverManager,
+ * FirefoxDriverManager, EdgeDriverManager) implement the factory method to create WebDriver instances specific
+ * to each browser.
+ * Additionally, the Singleton pattern ensures that only one instance of the WebDriverManager class exists
+ * throughout the application, providing a global point of access to the factory method.
  */
 public class WebDriverManager {
     
