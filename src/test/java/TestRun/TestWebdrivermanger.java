1629ac814f4b1aece2dcba4619419a63e08c7f86
prayashsdet <prayashqa@gmail.com>
2024-03-12 22:13:33 +0530

    c

--- src/test/java/TestRun/TestWebdrivermanger.java
+++ src/test/java/TestRun/TestWebdrivermanger.java
@@ -1,6 +1,8 @@
 package TestRun;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,5 +18,24 @@ public class TestWebdrivermanger {
 	    WebDriver firefoxDriver = WebDriverManager.getDriver("firefox");
 	    Assert.assertNotNull(firefoxDriver);
 	    // Perform test actions and assertions for Firefox
+	}
+	
+	@Test
+	public void testBrowserName() {
+		WebDriver chromeDriver = WebDriverManager.getDriver("chrome");
+		Assert.assertEquals(new ChromeOptions().getBrowserName(),"chrome");
+		
+		WebDriver firefoxDriver = WebDriverManager.getDriver("firefox");
+		Assert.assertEquals(new FirefoxOptions ().getBrowserName(),"firefox");
+	}
+	
+	public void testincognito() {
+		WebDriver chromeDriver = WebDriverManager.getDriver("chrome");
+		new ChromeOptions().addArguments("--incognito");
+		   Assert.assertTrue(new ChromeOptions().getCapabilityNames().contains("--incognito"));
+		
+		WebDriver firefoxDriver = WebDriverManager.getDriver("firefox");
+		new FirefoxOptions().addArguments("--headles");
+		Assert.assertTrue(new FirefoxOptions().getCapabilityNames().contains("--headless"));
 	}
 }
